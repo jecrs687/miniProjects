8dcf96ba07af07c63f116f335bbc5ca1aab62312
jecrs687 <emanuelcascone@gmail.com>
2020-03-07 12:15:41 -0300

    Auto-generated commit with gh-pages

--- border-creator/service-worker.js
+++ border-creator/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c6abf0adaf78e6e3575dd4232759559.js"
+  "/miniProjects/static/border-creator/precache-manifest.e345905180ef8541e69c5e842e5862b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/miniProjects/static/border-creator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
