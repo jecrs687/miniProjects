9620a97e8be21c6b8db7b1b0b93174c384d930cd
jecrs687 <emanuelcascone@gmail.com>
2020-03-07 10:18:32 -0300

    Auto-generated commit with gh-pages

---
+++ border-creator/precache-manifest.79c18939e5957574fd7ad1e118f4c7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82af42eb679b096b54351b2210d4917d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c9c2d63d62e3e5d2fbb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c342c8f8b7924fc72e79",
+    "url": "/static/js/2.f8ed56a2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f8ed56a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9c2d63d62e3e5d2fbb",
+    "url": "/static/js/main.c0ce9571.chunk.js"
+  },
+  {
+    "revision": "603374047bbd72b64775",
+    "url": "/static/js/runtime-main.02b65fd8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
