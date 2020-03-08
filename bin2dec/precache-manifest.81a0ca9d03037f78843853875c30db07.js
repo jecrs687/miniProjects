1bef92dc29022ff2d7b23a8f289b16263d1e9673
jecrs687 <emanuelcascone@gmail.com>
2020-03-08 10:22:23 -0300

    Auto-generated commit with gh-pages

---
+++ bin2dec/precache-manifest.81a0ca9d03037f78843853875c30db07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f40f6b61894ba869b767599820c938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "685017142488a0ce3e74",
+    "url": "/static/css/main.034fef6a.chunk.css"
+  },
+  {
+    "revision": "010c46b7c0643fb8d76f",
+    "url": "/static/js/2.966ad819.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.966ad819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685017142488a0ce3e74",
+    "url": "/static/js/main.467f2f32.chunk.js"
+  },
+  {
+    "revision": "647c3b1f197772929dd3",
+    "url": "/static/js/runtime-main.03a93ed9.js"
+  }
+]);
