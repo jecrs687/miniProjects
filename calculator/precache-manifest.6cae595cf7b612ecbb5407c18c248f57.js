b7fff271b6cabdaad40f4922105ede53ed7533a9
jecrs687 <emanuelcascone@gmail.com>
2020-03-08 16:01:40 -0300

    Auto-generated commit with gh-pages

---
+++ calculator/precache-manifest.6cae595cf7b612ecbb5407c18c248f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a7dd5db0bbd8f2854cf6f16b0c2cac",
+    "url": "/miniProjects/calculator/index.html"
+  },
+  {
+    "revision": "09fe3144724c328ddb8c",
+    "url": "/miniProjects/calculator/static/css/main.cecf8059.chunk.css"
+  },
+  {
+    "revision": "413d0cd3657c2dda0634",
+    "url": "/miniProjects/calculator/static/js/2.da67fee6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/miniProjects/calculator/static/js/2.da67fee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09fe3144724c328ddb8c",
+    "url": "/miniProjects/calculator/static/js/main.bf5753a6.chunk.js"
+  },
+  {
+    "revision": "052bad9dd411595e0208",
+    "url": "/miniProjects/calculator/static/js/runtime-main.f127f92f.js"
+  }
+]);
